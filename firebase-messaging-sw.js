a771ecd6154b0df2af8d29d0e2569672cd74b0f0
tejari49 <148905876+tejari49@users.noreply.github.com>
2026-03-01 22:30:49 +0100

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,7 +14,7 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // PWA Offline Caching
-const CACHE_NAME = 'onyx-v18';
+const CACHE_NAME = 'onyx-v20';
 const STATIC_ASSETS = [
   './manifest.json',
   './icon-192.png',
